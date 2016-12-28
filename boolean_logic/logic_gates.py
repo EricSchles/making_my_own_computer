e7f50e62a33343dd60103ac7521478abeeea58d4
Eric Schles <ericschles@gmail.com>
2016-12-28 08:59:37 -0500

    updating

--- boolean_logic/logic_gates.py
+++ boolean_logic/logic_gates.py
@@ -16,6 +16,37 @@ def multi_and(*args):
         result = result and elem
     return result
 
+def simple_or(x,y):
+    return x or y
+
+def multi_or(*args):
+    inputs = *args
+    result = inputs[0]
+    for elem in inputs[1:]:
+        result = result or elem
+    return result
+
+def simple_and_not(x,y):
+    return x and not y
+
+def multi_and_not(*args):
+    inputs = *args
+    result = inputs[0]
+    for elem in inputs[1:]:
+        result = result and not elem
+    return result
+
+def simple_not_and(x,y):
+    return (not x) and y
+
+def multi_not_and(*args):
+    inputs = *args
+    result = inputs[0]
+    for elem in inputs[1:]:
+        result = (not result) and elem
+    return result
+
+
 MG = nx.MultiGraph()
 
 
